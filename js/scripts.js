884f9ac62f33f3fc275701595dcd39c1935917a0
JoseGomezCereto <trabajo.jgomez@gmail.com>
2022-07-02 23:10:26 +0200

    Fully Functional. Language Change

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,26 @@ const toggleColors = document.getElementById("toggle-colors");
 
 const rootStyles = document.documentElement.style;
 
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+      
+        textToChange.innerHTML = texts[section][value];
+    }
+};
+
+flagsElement.addEventListener("click", (e) => {
+    changeLanguage(e.target.parentElement.dataset.language);
+});
+
 toggleTheme.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 
